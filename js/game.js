680f012d72291e581cb9ad2ce3a0a8eeba65651f
PeTcHeNkA <barashekshon72@gmail.com>
2023-05-17 04:39:01 +0300

    refactoring game.js..

--- js/game.js
+++ js/game.js
@@ -1,178 +1,180 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
-//load image
-let a = new Image(); a.src = 'img/ground.png';
-let b = new Image(); b.src = 'img/food.png';
-const imgur = {
-    ground: a,
-    food: b
-};
-//load music
-const music = {
+
+// Loading images
+let groundImg = new Image();
+groundImg.src = 'img/ground.png';
+
+let foodImg = new Image();
+foodImg.src = 'img/food.png';
+
+// Loading sounds
+const sound = {
     death: new Audio('sound/death.mp3'),
     eating: new Audio('sound/eating.mp3')
 };
-//load font
-var f = new FontFace('PressStart2P', 'url(font/PressStart2P.ttf)');
-f.load().then(function (font) {
-    document.fonts.add(font);
+
+// Loading font
+var font = new FontFace('PressStart2P', 'url(font/PressStart2P.ttf)');
+font.load().then(function (loadedFont) {
+    document.fonts.add(loadedFont);
 });
-//config and etc :D
-let box = 32;
-let score = 0;
-let food = [];
-let snake = [];
-let dir;
-//snake spawn
+
+// Constants and variables
+const boxSize = 32; // Cell size
+let score = 0; // Score
+let food = []; // Array for food
+let snake = []; // Array for snake
+let direction; // Direction of movement
+
+// Creating snake
 snake[0] = {
-    x: 9 * box,
-    y: 10 * box
+    x: 9 * boxSize,
+    y: 10 * boxSize
 };
-//mini-spawner
-toStarter(3)
-function toStarter(i) {
-    //carrot spawn with cooldown
-    toSpawn(0).then(() => {
-        toSpawn(1).then(() => {
-            toSpawn(2).then(() => {})
-        })
-    })
-}
-async function toSpawn(i) {
-    await new Promise((resolve, reject) => setTimeout(resolve, 2500));
-    for (let ms = 0; ms < snake.length; ms++) {
-        for (let ms1 = 0; ms1 != i;) {
-            let x1 = Math.floor((Math.random() * 17 + 1)) * box;
-            let y1 = Math.floor((Math.random() * 15 + 3)) * box;
-            if ((snake[ms].x != x1) && (snake[ms].y != y1)) {
-                food[i] = {
-                    x: x1,
-                    y: y1
-                }
-                ms1++
+
+// Function for creating food with delay and checking for overlap with snake
+function createFood(delay) {
+    setTimeout(function () {
+        let foodX = Math.floor((Math.random() * 17 + 1)) * boxSize;
+        let foodY = Math.floor((Math.random() * 15 + 3)) * boxSize;
+        let isOverlapping = false;
+        for (let i = 0; i < snake.length; i++) {
+            if (snake[i].x == foodX && snake[i].y == foodY) {
+                isOverlapping = true;
+                break;
             }
         }
-    }
+        if (!isOverlapping) {
+            food.push({
+                x: foodX,
+                y: foodY
+            });
+        } else {
+            createFood(delay); // Repeat the attempt if there is an overlap
+        }
+    }, delay);
 }
-//eventor register
-/*
-var event = null;
 
-document.addEventListener('mousedown', function (e) {
-    event = e;
-});
-document.addEventListener('mousemove', function (e) {
-    if (event) {
-        if(e.screenX < event.screenX) {
-            dir = 'left';
-        }
-        else if(e.screenX > event.screenX) {
-            console.log('Право')
-            dir = 'right'
-        }
-        else if(e.screenY < event.screenY) {
-            dir = 'up';
-        }
-        else if(e.screenY > event.screenY) {
-            dir = 'down';
-        }
-    }
-});
-document.addEventListener('mouseup', function (e) {
-    event = null;
-}); */
+// Function for creating three units of food at the start of the game
+function createFoodAtStart() {
+    createFood(0);
+    createFood(2500);
+    createFood(5000);
+}
 
-document.addEventListener('keydown', direction);
-function direction(event) {
-    if ((event.keyCode == 68 || event.keyCode == 39) && dir != 'left')
-        dir = 'right';
-    else if ((event.keyCode == 83 || event.keyCode == 40) && dir != 'up')
-        dir = 'down';
-    else if ((event.keyCode == 65 || event.keyCode == 37) && dir != 'right')
-        dir = 'left';
-    else if ((event.keyCode == 87 || event.keyCode == 38) && dir != 'down')
-        dir = 'up';
-    else if(event.keyCode == '82') {
-        location.reload()
+createFoodAtStart(); // Calling the function
+
+// Function for handling key presses
+function handleKeyDown(event) {
+    if ((event.keyCode == 68 || event.keyCode == 39) && direction != 'left')
+        direction = 'right';
+    else if ((event.keyCode == 83 || event.keyCode == 40) && direction != 'up')
+        direction = 'down';
+    else if ((event.keyCode == 65 || event.keyCode == 37) && direction != 'right')
+        direction = 'left';
+    else if ((event.keyCode == 87 || event.keyCode == 38) && direction != 'down')
+        direction = 'up';
+    else if (event.keyCode == '82') {
+        location.reload();
     }
 }
-//game helper
-function eatTail(head, arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (head.x == arr[i].x && head.y == arr[i].y) {
-            clearInterval(game);
-            gameOverStart();
+
+document.addEventListener('keydown', handleKeyDown);
+
+// Function for checking collision of snake head with body
+function checkCollision(head, body) {
+    for (let i = 0; i < body.length; i++) {
+        if (head.x == body[i].x && head.y == body[i].y) {
+            return true;
         }
     }
+    return false;
 }
-function gameOverStart() {
-    clearInterval(game);
+
+// Function for checking boundary of the field
+function checkBoundary(x, y, size) {
+    return x < size || x > size * 17 || y < size * 3 || y > size * 17;
+}
+
+// Function for ending the game
+function gameOver() {
+    clearInterval(gameLoop);
     ctx.fillStyle = 'white';
-    ctx.font = '55px PressStart2P';
-    ctx.textAlighn = 'center';
-    ctx.fillText('Game Over!', 40, 365);
-    music.death.play();
+    ctx.font = '52px PressStart2P';
+    ctx.textAlign = 'center';
+    ctx.fillText('Game Over!', canvas.width / 2, canvas.height / 2);
+    sound.death.play();
 }
-function checkFood(snakeX,snakeY,food) {
-    return (snakeX == food.x && snakeY == food.y) 
-}
-function checkerWall(snakeX, snakeY,box) {
-    return (snakeX < box || snakeX > box * 17 || snakeY < 3 * box || snakeY > box * 17)
-}
-//start game
-let game = setInterval(drawGame, 100);
 
+// Function for drawing the game
 function drawGame() {
-    //mannager snake x,y
+
+    // Getting the coordinates of the snake head
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
-    //draw score and ground
-    ctx.drawImage(imgur.ground, 0, 0);
+
+    // Drawing the background and score
+    ctx.drawImage(groundImg, 0, 0);
     ctx.fillStyle = 'white';
-    ctx.font = '32px PressStart2P'
-    ctx.fillText(score, box * 2.4, box * 1.7);
-    //draw snake and carrot
-    if(!checkerWall(snakeX,snakeY,box)) {
-        if(food[0] && !checkFood(snakeX,snakeY,food[0])) ctx.drawImage(imgur.food, food[0].x, food[0].y);
-        if(food[1] && !checkFood(snakeX,snakeY,food[1])) ctx.drawImage(imgur.food, food[1].x, food[1].y);
-        if(food[2] && !checkFood(snakeX,snakeY,food[2])) ctx.drawImage(imgur.food, food[2].x, food[2].y);
+    ctx.font = '32px PressStart2P';
+    ctx.fillText(score, boxSize * 2.4, boxSize * 1.7);
+
+    // Drawing the snake and food
+    if (!checkBoundary(snakeX, snakeY, boxSize)) {
+        for (let i = 0; i < food.length; i++) {
+            if (snakeX != food[i].x || snakeY != food[i].y) {
+                ctx.drawImage(foodImg, food[i].x, food[i].y);
+            }
+        }
         for (let i = 0; i < snake.length; i++) {
             ctx.fillStyle = i == 0 ? 'green' : '#00FF7F';
-            if(i+1 == snake.length && i != 0) ctx.fillStyle = 'red'
-            ctx.fillRect(snake[i].x+5, snake[i].y+5, box-8, box-8);
-            //ctx.beginPath();
-            //ctx.fillStyle = i == 0 ? '#3CB371' : ' #00FF7F';
-            //ctx.arc(snake[i].x+15, snake[i].y+15, box/2, 0, 2*Math.PI, false);
-            //ctx.fill();
+            if (i + 1 == snake.length && i != 0) ctx.fillStyle = 'red';
+            ctx.fillRect(snake[i].x + 5, snake[i].y + 5, boxSize - 8, boxSize - 8);
         }
     }
-    //checker eating --event
-    if(food[0] && checkFood(snakeX,snakeY,food[0])) eating(0);
-    else if(food[1] && checkFood(snakeX,snakeY,food[1])) eating(1);
-    else if(food[2] && checkFood(snakeX,snakeY,food[2])) eating(2);
-        else snake.pop()
 
-    function eating(i) {
-        food[i] = {
-            x: Math.floor((Math.random() * 17 + 1)) * box,
-            y: Math.floor((Math.random() * 15 + 3)) * box
+    // Checking if eating food and increasing snake length
+    let hasEaten = false;
+    for (let i = 0; i < food.length; i++) {
+        if (snakeX == food[i].x && snakeY == food[i].y) {
+            food.splice(i, 1); // Removing the eaten food from the array
+            createFood(2500); // Creating new food with delay
+            sound.eating.play(); // Playing the sound
+            score++; // Increasing the score
+            hasEaten = true;
         }
-        music.eating.play();
-        score++;
     }
-    //check wall to game over
-    if(checkerWall(snakeX,snakeY,box)) {
-        gameOverStart()
+    if (!hasEaten) {
+        snake.pop();
     }
 
-    if (dir == 'left') snakeX -= box;
-    if (dir == 'right') snakeX += box;
-    if (dir == 'up') snakeY -= box;
-    if (dir == 'down') snakeY += box;
+
+    // Checking if going out of bounds and ending the game
+    if (checkBoundary(snakeX, snakeY, boxSize)) {
+        gameOver();
+        return;
+    }
+
+    // Changing the coordinates of the snake head depending on the direction
+    if (direction == 'left') snakeX -= boxSize;
+    if (direction == 'right') snakeX += boxSize;
+    if (direction == 'up') snakeY -= boxSize;
+    if (direction == 'down') snakeY += boxSize;
+
     let newHead = {
         x: snakeX,
         y: snakeY
-    };
-    eatTail(newHead, snake);
-    snake.unshift(newHead);
+    }; // Creating a new head
+
+    // Checking if colliding with the body and ending the game
+    if (checkCollision(newHead, snake)) {
+        gameOver();
+        return;
+    }
+
+    snake.unshift(newHead); // Adding the new head to the beginning of the snake array
 }
+
+// Starting the game with an interval of 100 ms
+let gameLoop = setInterval(drawGame, 100);
